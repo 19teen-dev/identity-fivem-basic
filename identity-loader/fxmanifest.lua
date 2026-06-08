37b1b0f31811f2f9ca39b2e51378011047f67942
19teen-dev <extrimer2010@gmail.com>
2026-06-08 14:48:23 +1000

    Create fxmanifest.lua

---
+++ identity-loader/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'fReeed0m'
+description 'IDENTITY Loading screen'
+version '1.0.0'
+
+loadscreen 'ui/index.html'
+loadscreen_manual_shutdown 'yes'
+
+files {
+    'ui/index.html',
+    'ui/style.css',
+    'ui/script.js'
+}
