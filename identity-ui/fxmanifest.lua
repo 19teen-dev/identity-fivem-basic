c443e590fd05eba7fea9848ce04171257e73c27c
19teen-dev <extrimer2010@gmail.com>
2026-06-08 14:38:26 +1000

    Create fxmanifest.lua

---
+++ identity-ui/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+ui_page 'ui/dist/index.html'
+
+client_scripts {
+    'client.js'
+}
+
+files {
+    'ui/dist/index.html',
+    'ui/dist/**',
+    'ui/dist/assets/**',
+    'ui/dist/sounds/*.wav',
+    'ui/dist/sounds/*.ogg',
+    'ui/dist/sounds/*.mp3'
+}
